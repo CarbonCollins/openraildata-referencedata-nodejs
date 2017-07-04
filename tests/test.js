e7b788f0f94c24aeea4a86566a8e00cbf99ca865
carboncollins <steven@carboncollins.uk>
2017-07-04 06:57:04 +0100

    added extra functions for query search

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,22 @@
 
 const refData = require('../lib/refData');
 
-refData.connect('A!t4398htw4ho4jy');
+const V3 = require('../lib/models/v3');
+const V8 = require('../lib/models/v8');
 
-refData.on('ready', () => {
+// refData.connect('A!t4398htw4ho4jy');
+
+// refData.on('ready', () => {
   console.log('ready');
-  refData.getCurrentV3().then(() => {
+  refData.getLocalV3Json().then((curV3) => {
+    refData._v3 = new V3(curV3);
     console.log('v3 done');
-    return refData.getCurrentV8();
-  }).then(() => {
+    return refData.getLocalV8Json();
+  }).then((curV8) => {
+    refData._v8 = new V8(curV8);
     console.log('v8 done');
-    console.log(refData.v8.getTrainsOriginatingFrom('EUSTON'));
+    console.log(refData.v8.runSearch().origin('EUSTON').departsAt('16.43').intermediateStop('MKNSCEN'));
   }).catch((err) => {
     console.error(err);
   });
-});
+// });
